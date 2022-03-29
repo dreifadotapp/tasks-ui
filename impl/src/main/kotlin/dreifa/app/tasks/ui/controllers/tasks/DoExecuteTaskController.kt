e547ee4a6266eb984410de9027714565783e58f4
Ian Morgan <ian.j.morgan@gmail.com>
2022-03-29 18:21:37 +0100

    fix problem with serialisation of native kotlin types

--- impl/src/main/kotlin/dreifa/app/tasks/ui/controllers/tasks/DoExecuteTaskController.kt
+++ impl/src/main/kotlin/dreifa/app/tasks/ui/controllers/tasks/DoExecuteTaskController.kt
@@ -10,29 +10,39 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.body.form
 import org.http4k.routing.path
+import kotlin.reflect.KClass
 
-class DoExecuteTaskController(registry: Registry) : BaseController(){
+class DoExecuteTaskController(registry: Registry) : BaseController() {
     private val taskFactory = registry.get(TaskFactory::class.java)
     private val taskClient = registry.get(TaskClient::class.java)
     private val serialiser = JsonSerialiser()
     override fun handle(request: Request): Response {
-            val taskName = request.path("task")!!
+        val taskName = request.path("task")!!
 
-            val inputClazz = request.form("inputClazz")!!
-            val outputClazz = request.form("outputClazz")!!
-            val inputJson = request.form("inputJson")!!
-            val kClass = Class.forName(outputClazz).kotlin
+        val inputClazz = request.form("inputClazz")!!
+        val outputClazz = request.form("outputClazz")!!
+        val inputJson = request.form("inputJson")!!
+        val kClass = clazzFromName(outputClazz)
 
-            val input = serialiser.fromPacketPayload(inputJson, inputClazz)
+        val input = serialiser.fromPacketPayload(inputJson, inputClazz)
+        val ctx = SimpleClientContext()
+        val result = taskClient.execBlocking(ctx, taskName, input, kClass)
 
-            val ctx = SimpleClientContext()
-            val result = taskClient.execBlocking(ctx, taskName, input, kClass)
-
-            val json = serialiser.toPacketPayload(result)
-            return json(json)
+        val json = serialiser.toPacketPayload(result)
+        return json(json)
 
     }
 
-
-
+    private fun clazzFromName(clazzName: String): KClass<out Any> {
+        // annoyingly, Class.forName doesn't understand kotlin types
+        val remappedClazzName = when (clazzName) {
+            "kotlin.Int" -> "java.lang.Integer"
+            "kotlin.Long" -> "java.lang.Long"
+            "kotlin.Float" -> "java.lang.Float"
+            "kotlin.Double" -> "java.lang.Double"
+            "kotlin.Boolean" -> "java.lang.Boolean"
+            else -> clazzName
+        }
+        return Class.forName(remappedClazzName).kotlin
+    }
 }

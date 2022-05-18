2d217bb26d8c2ce887e1191d4796c8fefaea8204
Ian Morgan <ian.j.morgan@gmail.com>
2022-05-18 11:30:37 +0100

    improving OpenTelemetry - adding request parameters into telemetry

--- impl/src/main/kotlin/dreifa/app/tasks/ui/controllers/BaseController.kt
+++ impl/src/main/kotlin/dreifa/app/tasks/ui/controllers/BaseController.kt
@@ -12,22 +12,27 @@ import io.opentelemetry.api.trace.Tracer
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
+import org.http4k.routing.path
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 abstract class BaseController(registry: Registry) {
     private val tracer = registry.getOrNull(Tracer::class.java)
     private val provider = registry.getOrNull(OpenTelemetryProvider::class.java)
     fun runWithTelemetry(
-        trc: TelemetryRequestContext,
-        block: ((tec: TelemetryExecutionContext) -> Response)
+        trc: TelemetryRequestContext, block: ((tec: TelemetryExecutionContext) -> Response)
     ): Response {
         return if (provider != null && tracer != null) {
             val span = tracer.spanBuilder(trc.spanName).setSpanKind(SpanKind.SERVER).startSpan()
             span.setAttribute("http.method", trc.req.method.name)
+            extractUrlParametersForTelemetry(trc.spanName, trc.req).forEach {
+                span.setAttribute("http.param.${it.first}", it.second)
+            }
 
             try {
                 val telemetryContext = OpenTelemetryContext.fromSpan(span)
                 val result = block.invoke(
-                    TelemetryExecutionContext(trc.req, telemetryContext),
+                    TelemetryExecutionContext(telemetryContext),
                 )  // what if the result is streaming ? are we closing the span too soon?
                 completeSpan(span)
                 result
@@ -36,13 +41,35 @@ abstract class BaseController(registry: Registry) {
                 throw ex
             }
         } else {
-            block.invoke(TelemetryExecutionContext(trc.req, OpenTelemetryContext.root()))
+            block.invoke(TelemetryExecutionContext(OpenTelemetryContext.root()))
         }
     }
 
 
     protected fun buildBaseModel(@Suppress("UNUSED_PARAMETER") req: Request): MutableMap<String, Any> {
         return mutableMapOf("currentViewTask" to "view")
+    }
+
+    private fun extractUrlParametersForTelemetry(path: String, req: Request): List<Pair<String, String>> {
+        val parts = ArrayList<String>()
+        var capture = false
+        var buffer = StringBuffer()
+        path.chars().forEach {
+            if (it == '}'.code && capture) {
+                capture = false
+                parts.add(buffer.toString())
+                buffer = StringBuffer()
+                capture = false
+            }
+            if (capture) {
+                buffer.append(it.toChar())
+            }
+            if (it == '{'.code) {
+                capture = true
+            }
+        }
+
+        return parts.map { Pair(it, req.path(it) ?: "???") }
     }
 
     fun setMenuFlags(model: MutableMap<String, Any>, vararg flags: String) {
@@ -63,15 +90,11 @@ abstract class BaseController(registry: Registry) {
 
 
     fun html(content: String): Response {
-        return Response(Status.OK)
-            .header("content-type", "text/html")
-            .body(content)
+        return Response(Status.OK).header("content-type", "text/html").body(content)
     }
 
     fun json(content: String): Response {
-        return Response(Status.OK)
-            .header("content-type", "application/json")
-            .body(content)
+        return Response(Status.OK).header("content-type", "application/json").body(content)
     }
 
     fun templateEngine(): TemplateProcessor = templateEngine
@@ -99,6 +122,6 @@ abstract class BaseController(registry: Registry) {
     /**
      * The data the telemetry handler passes back to the code block
      */
-    data class TelemetryExecutionContext(val req: Request, val otc: OpenTelemetryContext)
+    data class TelemetryExecutionContext(val otc: OpenTelemetryContext)
 
 }
